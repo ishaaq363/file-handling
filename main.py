78682d5941aac9488e9f83a1dccb6f59d8f6a6c6
ishaaq363 <imatovu383@gmail.com>
2025-01-18 18:50:55 +0200

    file handling

---
+++ main.py
@@ -0,0 +1,12 @@
+file = open("abc.txt", "r")
+
+print(file.read())
+
+# filewrite = open("abc.txt", "w")
+
+# filewrite.write("gggggggggggggggggggggggggg")
+
+
+filewrite = open("abc.txt", "a")
+
+filewrite.write("ooooooooooooooooooooooooo")
---
+++ main2.py
@@ -0,0 +1,9 @@
+file = open("abc.txt", "r")
+print(file.read())
+
+file = open("abc.txt", "w")
+file.write("my name is ishaaq matovu")
+
+
+
+file.close
